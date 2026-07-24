2a53dc11fc0182f86781576058ff3d8f916c08c7
Scratchaker <105060449+Scratchaker@users.noreply.github.com>
2026-07-24 18:15:49 +0200

    1.0.4 Release

--- runWithProton.sh
+++ runWithProton.sh
@@ -14,7 +14,7 @@
 PROTON_ROOT="$HOME/.proton"
 STEAM_ROOT="$(realpath "$HOME/.steam/root")"
 PROTON_VER="Proton - Experimental"
-0.4=("/usr/share/steam")
+ADDITIONAL_PROTON_DIRS=("/usr/share/steam")
 STEAM_RUNTIME="$STEAM_ROOT/steamapps/common/SteamLinuxRuntime_sniper/run"
 USE_UNIFIED_PREFIX=0
 USE_MANGOHUD=0
